3782e5d9cf8fc26868263e15d3982f2a200d09b5
Romjan Ali <romjanvr5@gmail.com>
2025-02-16 23:50:58 +0600

    28.7 & 28.8 added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 const List = () => {
   const fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango", "Pine Apple"];
+
+  const [selctedIndex, setSelectedIndex] = useState(-1);
+  // const [name, setName] = useState("");
+
+  // // let selctedIndex = -1;
+  // const handleItemPrint = (index) => {
+
+  //   console.log(selctedIndex);
+  // };
 
   return (
     <div>
       <ul className="pl-6 list-decimal text-xl">
-        {fruits.map((fruit) => {
-          return <li key={fruit}>{fruit}</li>;
+        {fruits.map((fruit, index) => {
+          return (
+            <li
+              className={
+                selctedIndex === index ? "bg-blue-500 p-3 rounded-sm m-3" : ""
+              }
+              onClick={() => setSelectedIndex(index)}
+              key={fruit}
+            >
+              {fruit}
+            </li>
+          );
         })}
       </ul>
     </div>
