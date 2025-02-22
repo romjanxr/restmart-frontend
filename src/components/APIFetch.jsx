c70b2be09d8772ec6a3ad1317aa27fa80a86b73b
Romjan Ali <romjanvr5@gmail.com>
2025-02-23 00:37:49 +0600

    29.9 added

--- src/components/APIFetch.jsx
+++ src/components/APIFetch.jsx
@@ -1,16 +1,26 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 const APIFetch = () => {
   const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+
+  //   useEffect(() => {
+  //     fetch("https://jsonplaceholder.typicode.com/users")
+  //       .then((res) => res.json())
+  //       .then((data) => setUsers(data));
+  //   }, []);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/wusers")
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((data) => setUsers(data.data))
+      .catch((err) => setError(err.message));
   }, []);
 
   return (
     <div>
+      {error && <p className="bg-red-100 text-red-900">{error}</p>}
       <ul className="list-disc m-5">
         {users.map((user) => (
           <li key={user.id}>
