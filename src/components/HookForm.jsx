7380105cef55c106ae8b32d3e25880e6a80d4be6
Romjan Ali <romjanvr5@gmail.com>
2025-02-24 00:23:01 +0600

    30.5 added

--- src/components/HookForm.jsx
+++ src/components/HookForm.jsx
@@ -1,7 +1,14 @@
 import { useForm } from "react-hook-form";
 
 const HookForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors);
+
   const onSubmit = (data) => {
     console.log(data);
   };
@@ -17,11 +24,17 @@ const HookForm = () => {
             Name:
           </label>
           <input
-            {...register("name")}
+            {...register("name", { required: true, minLength: 5 })}
             id="name"
             type="text"
             className="w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md"
           />
+          {errors.name?.type === "required" && (
+            <span>This field is required</span>
+          )}
+          {errors.name?.type === "minLength" && (
+            <span>Minimum length is 5</span>
+          )}
         </div>
         <div className="mb-3">
           <label
@@ -31,11 +44,15 @@ const HookForm = () => {
             Age:
           </label>
           <input
-            {...register("age")}
+            {...register("age", { required: true, min: 18 })}
             id="age"
             type="number"
             className="w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md"
           />
+          {errors.age?.type === "required" && (
+            <span>This field is required</span>
+          )}
+          {errors.age?.type === "min" && <span>Minimum age is 18</span>}
         </div>
         <button className="px-3 py-2 text-white bg-blue-500 rounded-md">
           Submit
