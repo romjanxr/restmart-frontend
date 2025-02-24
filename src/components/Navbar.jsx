91082dde0958940de3b946a6b9ac6b46b2813bc0
Romjan Ali <romjanvr5@gmail.com>
2025-02-25 00:49:31 +0600

    30.1 & 30.3 added

--- src/components/Navbar.jsx
+++
@@ -1,5 +0,0 @@
-const Navbar = ({ cartItemsCount }) => {
-  return <div>{cartItemsCount}</div>;
-};
-
-export default Navbar;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router";
+import Home from "../pages/Home/Home";
+import About from "../pages/About";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route index element={<Home />}></Route>
+      <Route path="about" element={<About />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
