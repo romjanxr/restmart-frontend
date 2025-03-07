ff84cb3ec777d4b6d37da078445266660469ffba
Romjan Ali <romjanvr5@gmail.com>
2025-03-08 00:57:16 +0600

    34.3 pre-added

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,32 +1,20 @@
-import { createContext, useState } from "react";
-import apiClient from "../services/api-client";
+import { createContext } from "react";
+import useAuth from "../hooks/useAuth";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-
-  const getToken = () => {
-    const token = localStorage.getItem("authTokens");
-    return token ? JSON.parse(token) : null;
-  };
-
-  const [authTokens, setAuthTokens] = useState(getToken());
-
-  // Login User
-  const loginUser = async (email, password) => {
-    const response = await apiClient.post("/auth/jwt/create/", {
-      email,
-      password,
-    });
-    console.log(response.data);
-  };
+  const allContext = useAuth();
 
   return (
-    <AuthContext.Provider value={{ loginUser }}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={allContext}>{children}</AuthContext.Provider>
   );
 };
 
 export default AuthContext;
+
+/* 
+{a: 10, b: 20}
+a, b
+{a, b}
+*/
