b1bc692326682ae105b19700c9288522873fb7b2
Romjan Ali <romjanvr5@gmail.com>
2025-03-11 02:56:58 +0600

    35.4 added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,28 @@
+import { useForm } from "react-hook-form";
+import ProfileForm from "../components/Dashboard/Profile/ProfileForm";
+import { useState } from "react";
+import ProfileButtons from "../components/Dashboard/Profile/ProfileButtons";
+
 const Profile = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const {
+    register,
+    formState: { errors },
+  } = useForm();
   return (
     <div className="card w-full max-w-2xl mx-auto bg-base-100 shadow-xl">
       <div className="card-body">
         <h2 className="card-title text-2xl mb-4">Profile Information</h2>
+
+        <form action="">
+          <ProfileForm
+            register={register}
+            errors={errors}
+            isEditing={isEditing}
+          />
+
+          <ProfileButtons isEditing={isEditing} setIsEditing={setIsEditing} />
+        </form>
       </div>
     </div>
   );
